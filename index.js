aa0708070e89fcc224697578c48d1f151129ce56
Ankitprajapati7809 <ankitprajapati7809@gmail.com>
2025-01-25 13:39:55 +0530

    2 commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const connectDB = require('./config/db');
+// const express = require('express');
+// const connectDB = require('./config/db');
 
-const app = express();
+// const app = express();
 
-// Connect to database
-connectDB();
+// // Connect to database
+// connectDB();
 
-app.use(express.json());
+// app.use(express.json());
 
-// Define Routes
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+// // Define Routes
+// app.use('/', require('./routes/index'));
+// app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+// const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+changes in index.js
--- models/Url.js
+++ models/Url.js
@@ -8,3 +8,4 @@ const urlSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Url', urlSchema);
+// changes in url.js
